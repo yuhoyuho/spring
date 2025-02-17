6408dd31291be300f7da0be6c75007612813b8ed
yuhoyuho <leeyuho123@naver.com>
2025-02-17 17:09:49 +0900

    h2DataBase 연결

---
+++ hello.spring/hello.spring/README.md
@@ -0,0 +1,1 @@
+<h1>Spring Study</h1>
---
+++ hello.spring/hello.spring/src/main/java/hello/hellospring/spring/controller/MemberController.java
@@ -0,0 +1,45 @@
+package hello.hellospring.spring.controller;
+
+import hello.hellospring.spring.domain.Member;
+import hello.hellospring.spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired // dependency Injection (의존 관계 주입)
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
+    }
+}
